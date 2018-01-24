20524de239bf349187f30f2718ebedaf39709bef
Dark Stallion <gonzalez.ferny31@outlook.com>
2018-01-24 08:18:19 -0700

    Add files via upload

--- WindowScreen.cpp
+++ WindowScreen.cpp
@@ -1,13 +1,13 @@
 #include <allegro5\allegro.h>
 
 int main() {
-	ALLEGRO_DISPLAY *GameWindow = NULL;
-	al_init();
-	GameWindow = al_create_display(640, 400);
-	al_set_window_position(GameWindow, 200, 200);
-	al_set_window_title(GameWindow, "Ferny's Game");
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-	al_flip_display();
-	al_rest(10);
-	al_destroy_display(GameWindow);
+	ALLEGRO_DISPLAY *GameWindow = NULL; //Create a Display Window
+	al_init(); //Set up Allegro
+	GameWindow = al_create_display(640, 400); //Create Your Game Window
+	al_set_window_position(GameWindow, 200, 200); //Positions Widnow on Screen
+	al_set_window_title(GameWindow, "Ferny's Game"); //Window Title
+	al_clear_to_color(al_map_rgb(0, 0, 0)); //Set it's Color
+	al_flip_display(); //Write whatever is in memory to the screen
+	al_rest(10); //Pauses Game
+	al_destroy_display(GameWindow); //Clear Memory
 }
